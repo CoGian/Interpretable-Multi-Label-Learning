716f3f1f9731182c34d50269782e13945cf02f03
CoGian <kgiantsios@atypon.com>
2021-11-11 21:17:25 +0200

    testing lrp

---
+++ download_models.sh
@@ -0,0 +1,2 @@
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1r4yCkreBOaJEorxMiCn4tiEhwZMMiBzm' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1VWNzfVFf3jYu4T5UzCbcMHny7dOEraae" -O Datasets.zip && rm -rf /tmp/cookies.txt
+unzip HoC_models.zip
