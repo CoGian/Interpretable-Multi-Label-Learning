c9ccaf94a8da8aa1a6e8e9c3fc5f009c39fd322d
CoGian <kgiantsios@atypon.com>
2021-11-17 21:19:49 +0200

    fix ig testing

--- IG_BERT_explainability/explainer.py
+++ IG_BERT_explainability/explainer.py
@@ -113,7 +113,7 @@ class BaseExplainer(ABC):
             raise NotImplementedError("Lists of text are not currently supported.")
 
         text_ids = self.encode(text)
-        input_ids = self.tokenizer.encode(text, add_special_tokens=True)
+        input_ids = self.tokenizer.encode(text, add_special_tokens=True, max_length=512, truncation=True)
 
         # if no special tokens were added
         if len(text_ids) == len(input_ids):
