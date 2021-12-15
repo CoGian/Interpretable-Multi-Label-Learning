reformat fix scaling, include percentile thresholding

IG_BERT_explainability/multi_label_sequence_classification.py
@@ -1,6 +1,7 @@
 import warnings
 from typing import Dict, List, Optional, Tuple, Union
 
+import numpy as np
 import torch
 from torch.nn.modules.sparse import Embedding
 from transformers import PreTrainedModel, PreTrainedTokenizer
@@ -79,11 +80,28 @@ class MultiLabelSequenceClassificationExplainer(BaseExplainer):
 
     @property
     def word_attributions(self) -> list:
-        "Returns the word attributions for model and the text provided. Raises error if attributions not calculated."
+        '''
+        Returns the scaled (0,1) word attributions for model and the text provided. Raises error if attributions not calculated.
+        :return: word_attributions
+        '''
+
         word_attributions_list = []
         for attributions in self.attributions_list:
             if attributions is not None:
-                word_attributions_list.append(attributions.word_attributions)
+                word_attributions = []
+                words = []
+                for word, value in attributions.word_attributions:
+
+                    if value < 0:
+                        word_attributions.append(0)
+                    else:
+                        word_attributions.append(value)
+                    words.append(word)
+                word_attributions = np.array(word_attributions)
+                scaled_word_attributions = (word_attributions - word_attributions.min()) / (word_attributions.max() - word_attributions.min())
+                word_attributions = [(word, value) for word, value in zip(words, scaled_word_attributions)]
+
+                word_attributions_list.append(word_attributions)
             else:
                 raise ValueError(
                     "Attributions have not yet been calculated. Please call the explainer on text first."

LRP_BERT_explainability/BERT/layers_with_rel_prop.py
@@ -51,10 +51,8 @@ class RelPropSimple(RelProp):
         S = safe_divide(R, Z)
         C = self.gradprop(Z, self.X, S)
 
-        if torch.is_tensor(self.X) == False:
-            outputs = []
-            outputs.append(self.X[0] * C[0])
-            outputs.append(self.X[1] * C[1])
+        if not torch.is_tensor(self.X):
+            outputs = [self.X[0] * C[0], self.X[1] * C[1]]
         else:
             outputs = self.X * (C[0])
         return outputs

test_ig.py
@@ -1,14 +1,21 @@
+import argparse
+
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 from IG_BERT_explainability.multi_label_sequence_classification import MultiLabelSequenceClassificationExplainer
 import json
 import re
 from sklearn import preprocessing
-import numpy as np
 import torch
 from tqdm import tqdm
-import pandas as pd
+from utils.metrics import update_sentence_metrics, print_metrics
 
 if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--threshold', '-t', help='The threshold of accepting a sentence as rationale')
+    args = parser.parse_args()
+    threshold = args.threshold
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
@@ -29,11 +36,9 @@ if __name__ == '__main__':
     with open("Datasets/HoC/val.json", "r") as fval:
         val_dataset = json.load(fval)
 
-    tp = 0
-    fp = 0
-    tn = 0
-    fn = 0
+    scores = {"tp": 0, "fp": 0, "tn": 0, "fn": 0}
     scores_per_label = {label: {"tp": 0, "fp": 0, "tn": 0, "fn": 0} for label in topics}
+
     for item in tqdm(val_dataset):
 
         text = item["text"].lower()
@@ -43,7 +48,8 @@ if __name__ == '__main__':
         gold_labels = mlb.transform([item["labels"]])[0]
         gold_indexes = [i for i, j in enumerate(gold_labels) if j >= 1]
 
-        word_attributions = multilabel_explainer(text=text, n_steps=8, internal_batch_size=4)
+        word_attributions_per_pred_class = multilabel_explainer(text=text, n_steps=1, internal_batch_size=1)
+
 
         output_indexes = multilabel_explainer.selected_indexes
 
@@ -53,58 +59,23 @@ if __name__ == '__main__':
                     continue
                 sentences_expl = []
                 sent_expl = []
-                for word in enumerate(word_attributions[i]):
+                for word in enumerate(word_attributions_per_pred_class[i]):
                     sent_expl.append(word[1][1])
                     if word[1][0] == ".":
                         sentences_expl.append(sent_expl)
                         sent_expl = []
 
-                sent_scores = []
-                for sent_expl in sentences_expl:
-                    sent_scores.append(np.mean(sent_expl))
-
-                sent_scores = np.array(sent_scores)
-                sent_scores = (sent_scores - sent_scores.min()) / (sent_scores.max() - sent_scores.min())
-
-                one_hot = np.zeros((1, len(gold_labels)), dtype=np.float32)
-                one_hot[0, output_index] = 1
-
-                gold_label = mlb.inverse_transform(one_hot)[0][0]
-
-                for index, score in enumerate(sent_scores):
-                    if score > 0.9:
-                        if gold_label in item["labels_per_sentence"][index]:
-                            tp += 1
-                            scores_per_label[gold_label]["tp"] += 1
-                        else:
-                            fp += 1
-                            scores_per_label[gold_label]["fp"] += 1
-                    else:
-                        if gold_label in item["labels_per_sentence"][index]:
-                            fn += 1
-                            scores_per_label[gold_label]["fn"] += 1
-                        else:
-                            tn += 1
-                            scores_per_label[gold_label]["tn"] += 1
+                update_sentence_metrics(
+                    sentences_expl,
+                    gold_labels,
+                    output_index,
+                    scores,
+                    scores_per_label,
+                    mlb,
+                    item,
+                    threshold
+                )
         except IndexError:
             print("4444 error for item: ", item["pmid"])
 
-    print("tp", tp)
-    print("fp", fp)
-    print("tn", tn)
-    print("fn", fn)
-    recall = tp / (tp+fn)
-    precision = tp / (tp+fp)
-    print("Recall: ", recall)
-    print("Precision: ", precision)
-    print("F1: ", (2*recall*precision)/(recall+precision)) #micro
-
-    metrics_per_labels = {}
-    for label in topics:
-        recall = scores_per_label[label]["tp"] / (scores_per_label[label]["tp"] + scores_per_label[label]["fn"])
-        precision = scores_per_label[label]["tp"] / (scores_per_label[label]["tp"] + scores_per_label[label]["fp"])
-        f1 = (2*recall*precision)/(recall+precision)
-        metrics_per_labels[label] = {"recall": recall, "precision": precision, "f1": f1}
-
-    with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-        print(pd.DataFrame(metrics_per_labels))
+    print_metrics(scores, scores_per_label, topics)

test_lrp.py
@@ -1,16 +1,20 @@
+import argparse
 import json
 import re
-
 from transformers import AutoTokenizer
 from LRP_BERT_explainability.BERT.BertForMultiLabelSequenceClassification import BertForMultiLabelSequenceClassification
 from LRP_BERT_explainability.ExplanationGenerator import Generator
 from sklearn import preprocessing
-import numpy as np
 import torch
 from tqdm import tqdm
-import pandas as pd
+from utils.metrics import update_sentence_metrics, print_metrics
 
 if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument(
+		'--threshold', '-t', help='The threshold of accepting a sentence as rationale')
+	args = parser.parse_args()
+	threshold = args.threshold
 
 	device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
@@ -32,11 +36,9 @@ if __name__ == '__main__':
 	with open("Datasets/HoC/val.json", "r") as fval:
 		val_dataset = json.load(fval)
 
-	tp = 0
-	fp = 0
-	tn = 0
-	fn = 0
+	scores = {"tp": 0, "fp": 0, "tn": 0, "fn": 0}
 	scores_per_label = {label: {"tp": 0, "fp": 0, "tn": 0, "fn": 0} for label in topics}
+
 	for item in tqdm(val_dataset):
 
 		text = item["text"].lower()
@@ -48,9 +50,9 @@ if __name__ == '__main__':
 		gold_labels = mlb.transform([item["labels"]])[0]
 		gold_indexes = [i for i, j in enumerate(gold_labels) if j >= 1]
 
-		word_attributions, output, output_indexes = explanations.generate_LRP(input_ids=input_ids,
-																			  attention_mask=attention_mask,
-																			  start_layer=0)
+		word_attributions_per_pred_class, output, output_indexes = explanations.generate_LRP(input_ids=input_ids,
+																							 attention_mask=attention_mask,
+																							 start_layer=0)
 		try:
 			for i, output_index in enumerate(output_indexes):
 				if output_index not in gold_indexes:
@@ -58,58 +60,23 @@ if __name__ == '__main__':
 				sentences_expl = []
 				sent_expl = []
 				for index, id in enumerate(input_ids[0]):
-					sent_expl.append(word_attributions[i][index].cpu().detach().numpy())
+					sent_expl.append(word_attributions_per_pred_class[i][index].cpu().detach().numpy())
 					if id.cpu().detach().numpy() == 1012:
 						sentences_expl.append(sent_expl)
 						sent_expl = []
-				sent_scores = []
-				for sent_expl in sentences_expl:
-					sent_scores.append(np.mean(sent_expl))
 
-				sent_scores = np.array(sent_scores)
-				sent_scores = (sent_scores - sent_scores.min()) / (sent_scores.max() - sent_scores.min())
+				update_sentence_metrics(
+					sentences_expl,
+					gold_labels,
+					output_index,
+					scores,
+					scores_per_label,
+					mlb,
+					item,
+					threshold
+				)
 
-				one_hot = np.zeros((1, len(gold_labels)), dtype=np.float32)
-				one_hot[0, output_index] = 1
-
-				gold_label = mlb.inverse_transform(one_hot)[0][0]
-
-				for index, score in enumerate(sent_scores):
-					if score > 0.9:
-						if gold_label in item["labels_per_sentence"][index]:
-							tp += 1
-							scores_per_label[gold_label]["tp"] += 1
-						else:
-							fp += 1
-							scores_per_label[gold_label]["fp"] += 1
-					else:
-						if gold_label in item["labels_per_sentence"][index]:
-							fn += 1
-							scores_per_label[gold_label]["fn"] += 1
-						else:
-							tn += 1
-							scores_per_label[gold_label]["tn"] += 1
 		except IndexError:
 			print("4444 error for item: ", item["pmid"])
 
-	print("tp", tp)
-	print("fp", fp)
-	print("tn", tn)
-	print("fn", fn)
-	recall = tp / (tp+fn)
-	precision = tp / (tp+fp)
-	print("Recall: ", recall)
-	print("Precision: ", precision)
-	print("F1: ", (2*recall*precision)/(recall+precision)) #micro
-
-	metrics_per_labels = {}
-	for label in topics:
-		recall = scores_per_label[label]["tp"] / (scores_per_label[label]["tp"] + scores_per_label[label]["fn"])
-		precision = scores_per_label[label]["tp"] / (scores_per_label[label]["tp"] + scores_per_label[label]["fp"])
-		f1 = (2*recall*precision)/(recall+precision)
-		metrics_per_labels[label] = {"recall": recall, "precision": precision, "f1": f1}
-
-	with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-		print(pd.DataFrame(metrics_per_labels))
-
-
+	print_metrics(scores, scores_per_label, topics)

test_raw_attn.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import re
 
@@ -11,6 +12,11 @@ from tqdm import tqdm
 
 
 if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument(
+		'--threshold', '-t', help='The threshold of accepting a sentence as rationale')
+	args = parser.parse_args()
+	threshold = args.threshold
 
 	device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
@@ -67,7 +73,8 @@ if __name__ == '__main__':
 					scores,
 					scores_per_label,
 					mlb,
-					item
+					item,
+					threshold
 				)
 		except IndexError:
 			print("4444 error for item: ", item["pmid"])

utils/metrics.py
@@ -2,21 +2,20 @@ import numpy as np
 import pandas as pd
 
 
-def update_sentence_metrics(sentences_expl, gold_labels, output_index, scores, scores_per_label, mlb, item):
+def update_sentence_metrics(sentences_expl, gold_labels, output_index, scores, scores_per_label, mlb, item, threshold=90):
 	sent_scores = []
 	for sent_expl in sentences_expl:
 		sent_scores.append(np.mean(sent_expl))
 
 	sent_scores = np.array(sent_scores)
-	sent_scores = (sent_scores - sent_scores.min()) / (sent_scores.max() - sent_scores.min())
-
+	percentile_threshold = np.percentile(sent_scores, threshold)
 	one_hot = np.zeros((1, len(gold_labels)), dtype=np.float32)
 	one_hot[0, output_index] = 1
 
 	gold_label = mlb.inverse_transform(one_hot)[0][0]
 
 	for index, score in enumerate(sent_scores):
-		if score > 0.9:
+		if score > percentile_threshold:
 			if gold_label in item["labels_per_sentence"][index]:
 				scores['tp'] += 1
 				scores_per_label[gold_label]["tp"] += 1